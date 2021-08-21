871411c08e22471298095ddb9d1dba273332de63
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-08-21 19:23:44 +0300

    Started new App and initialized packages / add sort-by function and fetch API key request

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default class Filters extends React.Component {
+  render() {
+    const {
+      filters: { sort_by },
+      onChangeFilters,
+    } = this.props;
+
+    return (
+      <form className="mb-3">
+        <div className="form-group">
+          <label htmlFor="sort_by">Сортировать по:</label>
+          <select
+            id="sort_by"
+            name="sort_by"
+            className="form-control"
+            value={sort_by}
+            onChange={onChangeFilters}
+          >
+            <option value="popularity.desc">Популярные по убыванию</option>
+            <option value="popularity.asc">Популярные по возростанию</option>
+            <option value="vote_average.desc">Рейтинг по убыванию</option>
+            <option value="vote_average.asc">Рейтинг по возростанию</option>
+          </select>
+        </div>
+      </form>
+    );
+  }
+}
---
+++ src/components/Movies/MoviesList.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import MovieItem from './MovieItem';
+import { API_URL, API_KEY_3 } from '../../api/api';
+
+export default class MovieList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: [],
+    };
+  }
+
+  getMovies = filters => {
+    const { sort_by } = filters;
+    const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&language=ru-RU&sort_by=${sort_by}`;
+    fetch(link)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          movies: data.results,
+        });
+      });
+  };
+
+  componentDidMount() {
+    this.getMovies(this.props.filters);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.filters.sort_by !== prevProps.filters.sort_by) {
+      this.getMovies(this.props.filters);
+    }
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div className="row">
+        {movies.map(movie => {
+          return (
+            <div key={movie.id} className="col-6 mb-4">
+              <MovieItem item={movie} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
