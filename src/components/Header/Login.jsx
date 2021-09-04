b5f4edb0c585717ae4c4feb089af7c77ccdc4f6d
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-09-05 00:00:51 +0300

    started login form component added fetch request

--- src/components/Header/Login.jsx
+++ src/components/Header/Login.jsx
@@ -1,57 +1,24 @@
 import React, { Component } from 'react';
 import { API_URL, API_KEY_3 } from '../../api/api';
+import { Modal, ModalBody } from 'reactstrap';
+import LoginForm from './LoginForm';
 
 export class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  toggleModal = () => {
+    this.setState(prevState => ({
+      showModal: !prevState.showModal,
+    }));
+  };
+
   sendPromise = async () => {
-    // const getRequestToken = () => {
-    //   return new Promise((resolve, reject) => {
-    //     fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
-    //       .then(response => {
-    //         if (response.status < 400) {
-    //           return response.json();
-    //         } else {
-    //           throw response;
-    //         }
-    //       })
-    //       .then(data => {
-    //         resolve(data);
-    //       })
-    //       .catch(response => {
-    //         response.json().then(error => {
-    //           reject(error);
-    //         });
-    //       });
-    //   });
-    // };
-
-    // const validateWithLogin = body => {
-    //   return new Promise((resolve, reject) => {
-    //     fetch(`${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`, {
-    //       method: 'POST',
-    //       mode: 'cors',
-    //       headers: {
-    //         'Content-type': 'application/json',
-    //       },
-    //       body: JSON.stringify(body),
-    //     })
-    //       .then(response => {
-    //         if (response.status < 400) {
-    //           return response.json();
-    //         } else {
-    //           throw response;
-    //         }
-    //       })
-    //       .then(data => {
-    //         resolve(data);
-    //       })
-    //       .catch(response => {
-    //         response.json().then(error => {
-    //           reject(error);
-    //         });
-    //       });
-    //   });
-    // };
-
     const fetchApi = (url, options = {}) => {
       return new Promise((resolve, reject) => {
         fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
@@ -89,7 +56,6 @@ export class Login extends Component {
         }),
       }
     );
-    console.log(result);
     const { request_token } = await fetchApi(
       `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,
       {
@@ -103,85 +69,19 @@ export class Login extends Component {
         }),
       }
     );
-
-    console.log(request_token);
   };
-
-  //   fetchApi(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
-  //     .then(data => {
-  //       return fetchApi(
-  //         `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`,
-  //         {
-  //           method: 'POST',
-  //           mode: 'cors',
-  //           headers: {
-  //             'Content-type': 'application/json',
-  //           },
-  //           body: JSON.stringify({
-  //             username: 'kbogorodsky',
-  //             password: 'JirWOW19IWNLswag06',
-  //             request_token: data.request_token,
-  //           }),
-  //         }
-  //       );
-  //     })
-  //     .then(data => {
-  //       return fetchApi(`${API_URL}/authentication/session/new?api_key=${API_KEY_3}`, {
-  //         method: 'POST',
-  //         mode: 'cors',
-  //         headers: {
-  //           'Content-type': 'application/json',
-  //         },
-  //         body: JSON.stringify({
-  //           request_token: data.request_token,
-  //         }),
-  //       });
-  //     })
-  //     .then(data => {
-  //       console.log(data);
-  //     })
-  //     .catch(error => console.warn(error));
-  // };
-
-  //   fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       fetch(`${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`, {
-  //         method: 'POST',
-  //         mode: 'cors',
-  //         headers: {
-  //           'Content-type': 'application/json',
-  //         },
-  //         body: JSON.stringify({
-  //           username: 'kbogorodsky',
-  //           password: 'JirWOW19IWNLswag06',
-  //           request_token: data.request_token,
-  //         }),
-  //       })
-  //         .then(response => response.json())
-  //         .then(data => {
-  //           fetch(`${API_URL}/authentication/session/new?api_key=${API_KEY_3}`, {
-  //             method: 'POST',
-  //             mode: 'cors',
-  //             headers: {
-  //               'Content-type': 'application/json',
-  //             },
-  //             body: JSON.stringify({
-  //               request_token: data.request_token,
-  //             }),
-  //           })
-  //             .then(response => response.json())
-  //             .then(data => console.log(data));
-  //         });
-  //     });
-  // };
 
   render() {
     return (
       <div>
-        <button className="btn btn-success" type="button" onClick={this.sendPromise}>
+        <button className="btn btn-success" type="button" onClick={this.toggleModal}>
           Login
         </button>
+        <Modal isOpen={this.state.showModal} toggle={this.toggleModal}>
+          <ModalBody>
+            <LoginForm />
+          </ModalBody>
+        </Modal>
       </div>
     );
   }
